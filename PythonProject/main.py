8641a8d45274533a7b0e1cfe033c585e6783fc36
Niv-J-Crissy <clopteryt@gmail.com>
2026-04-21 13:50:47 +0300

    Add files via upload

---
+++ PythonProject/main.py
@@ -0,0 +1,5 @@
+#Basic Band Name GENERATOR
+print("Welcome to Band name generator")
+city = input("What city did you grow up in?\n")
+pet_name = input("What is your pet name?\n")
+print("Your band name could be " + city + " " + pet_name)
